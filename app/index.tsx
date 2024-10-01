91d2d411751ad481032c93ff095f1dafce15cf72
Mitheel Ramdaw <mitheelramdaw@gmail.com>
2024-10-01 18:20:50 +0200

    Blank template

---
+++ app/index.tsx
@@ -0,0 +1,16 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text>Suiiii, get innn, it works!!!</Text>
+    </View>
+  );
+}
